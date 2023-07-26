548549af1d62d37f46c7336d62ceb8479295477e
davidacs10 <davidacs10.08@gmail.com>
2023-07-25 21:53:02 -0400

    Primeros pasos con python

---
+++ basic/00-hello.py
@@ -0,0 +1,14 @@
+# Esto es un comentario en python
+"""
+Esto es un comentario
+Multilinea en python
+"""
+
+print("Hola Mundo")  # Primer hola mundo en python :)
+
+# Tipos de datos en python
+print(type("Hola Mundo"))  # Dato tipo "str"
+print(type(1))  # Dato tipo "int"
+print(type(3.5))  # Dato tipo "float"
+print(type(3 + 4j))  # Dato tipo "complex"
+print(type(False))  # Dato tipo "bool"
