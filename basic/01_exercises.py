9187fbbae0dfa049047e42b8e4aa2576001a8853
davidacs10 <davidacs10.08@gmail.com>
2023-08-28 11:04:14 -0400

    Ejercicio de variables

---
+++ basic/01_exercises.py
@@ -0,0 +1,50 @@
+### Exercise: Level 1###
+
+first_name = "David"
+print(type(first_name))
+print(len(first_name))
+last_name = "Campos"
+print(type(last_name))
+print(len(last_name))
+full_name = first_name + last_name
+print(type(full_name))
+country = "Venezuela"
+print(type(country))
+city = "El Tigre"
+print(type(city))
+age = 26
+print(type(age))
+year = 2023
+print(type(year))
+is_married = False
+print(type(is_married))
+is_true = True
+print(type(is_true))
+is_light_on = True
+print(type(is_light_on))
+first_name, last_name, country, age, year = ("David", "Campos", "Venezuela", 26, 2023)
+num_one = 5
+num_two = 4
+suma = num_one + num_two
+resta = num_two - num_one
+multiply = num_two * num_one
+divide = num_one / num_two
+modulus = num_two % num_one
+exp = num_one**num_two
+floor_division = num_one // num_two
+print(exp)
+print(floor_division)
+radio = float(input("Ingrese el radio de un circulo:"))
+area_of_circle = 3.14 * (radio**2)
+circum_of_circle = 2 * 3.14 * radio
+print(area_of_circle)
+print(circum_of_circle)
+
+first_name = input("Ingrese su nombre: ")
+last_name = input("Ingrese su apellido: ")
+country = input("Ingrese su pais: ")
+age = input("Ingrese su edad: ")
+print(first_name)
+print(last_name)
+print(country)
+print(age)
