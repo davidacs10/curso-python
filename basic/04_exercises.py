4a9cb7a7db1fc87c76b9fc079689bdc8069e9efc
davidacs10 <davidacs10.08@gmail.com>
2023-09-11 20:33:48 -0400

    04_exercises.py complete

--- basic/04_exercises.py
+++ basic/04_exercises.py
@@ -101,4 +101,250 @@ del it_companies
 # 26 Join the following lists:
 front_end = ["HTML", "CSS", "JS", "React", "Redux"]
 back_end = ["Node", "Express", "MongoDB"]
-print(front_end.extend(back_end))
+front_end.extend(back_end)
+print(front_end)
+
+# 27 After joining the lists in question 26.
+# Copy the joined list and assign it to a variable full_stack. Then insert Python and SQL after Redux.
+full_stack = ["Python", "SQL", "Redux"]
+front_end.extend(full_stack)
+print(front_end)
+
+# 28 The following is a list of 10 students ages:
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+# Sort the list and find the min and max age
+ages.sort()
+print(ages)
+# Add the min age and the max age again to the list
+ages.append(19)
+ages.append(26)
+print(ages)
+# Find the median age (one middle item or two middle items divided by two)
+print(ages.index(24))
+# Find the average age (sum of all items divided by their number )
+average_age = sum(ages) / len(ages)
+print(average_age)
+# Find the range of the ages (max minus min)
+min_list = min(ages)
+max_list = max(ages)
+print(min_list, max_list)
+# Compare the value of (min - average) and (max - average), use abs() method
+min_average = abs(min_list - average_age)
+max_average = abs(max_list - average_age)
+compare_value = min_average > max_average
+print(min_average, max_average)
+print(compare_value)
+
+# 29 Find the middle country(ies) in the countries list
+countries = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Antigua and Barbuda",
+    "Argentina",
+    "Armenia",
+    "Australia",
+    "Austria",
+    "Azerbaijan",
+    "Bahamas",
+    "Bahrain",
+    "Bangladesh",
+    "Barbados",
+    "Belarus",
+    "Belgium",
+    "Belize",
+    "Benin",
+    "Bhutan",
+    "Bolivia",
+    "Bosnia and Herzegovina",
+    "Botswana",
+    "Brazil",
+    "Brunei",
+    "Bulgaria",
+    "Burkina Faso",
+    "Burundi",
+    "Cambodia",
+    "Cameroon",
+    "Canada",
+    "Cape Verde",
+    "Central African Republic",
+    "Chad",
+    "Chile",
+    "China",
+    "Colombi",
+    "Comoros",
+    "Congo (Brazzaville)",
+    "Congo",
+    "Costa Rica",
+    "Cote d'Ivoire",
+    "Croatia",
+    "Cuba",
+    "Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Djibouti",
+    "Dominica",
+    "Dominican Republic",
+    "East Timor (Timor Timur)",
+    "Ecuador",
+    "Egypt",
+    "El Salvador",
+    "Equatorial Guinea",
+    "Eritrea",
+    "Estonia",
+    "Ethiopia",
+    "Fiji",
+    "Finland",
+    "France",
+    "Gabon",
+    "Gambia, The",
+    "Georgia",
+    "Germany",
+    "Ghana",
+    "Greece",
+    "Grenada",
+    "Guatemala",
+    "Guinea",
+    "Guinea-Bissau",
+    "Guyana",
+    "Haiti",
+    "Honduras",
+    "Hungary",
+    "Iceland",
+    "India",
+    "Indonesia",
+    "Iran",
+    "Iraq",
+    "Ireland",
+    "Israel",
+    "Italy",
+    "Jamaica",
+    "Japan",
+    "Jordan",
+    "Kazakhstan",
+    "Kenya",
+    "Kiribati",
+    "Korea, North",
+    "Korea, South",
+    "Kuwait",
+    "Kyrgyzstan",
+    "Laos",
+    "Latvia",
+    "Lebanon",
+    "Lesotho",
+    "Liberia",
+    "Libya",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macedonia",
+    "Madagascar",
+    "Malawi",
+    "Malaysia",
+    "Maldives",
+    "Mali",
+    "Malta",
+    "Marshall Islands",
+    "Mauritania",
+    "Mauritius",
+    "Mexico",
+    "Micronesia",
+    "Moldova",
+    "Monaco",
+    "Mongolia",
+    "Morocco",
+    "Mozambique",
+    "Myanmar",
+    "Namibia",
+    "Nauru",
+    "Nepal",
+    "Netherlands",
+    "New Zealand",
+    "Nicaragua",
+    "Niger",
+    "Nigeria",
+    "Norway",
+    "Oman",
+    "Pakistan",
+    "Palau",
+    "Panama",
+    "Papua New Guinea",
+    "Paraguay",
+    "Peru",
+    "Philippines",
+    "Poland",
+    "Portugal",
+    "Qatar",
+    "Romania",
+    "Russia",
+    "Rwanda",
+    "Saint Kitts and Nevis",
+    "Saint Lucia",
+    "Saint Vincent",
+    "Samoa",
+    "San Marino",
+    "Sao Tome and Principe",
+    "Saudi Arabia",
+    "Senegal",
+    "Serbia and Montenegro",
+    "Seychelles",
+    "Sierra Leone",
+    "Singapore",
+    "Slovakia",
+    "Slovenia",
+    "Solomon Islands",
+    "Somalia",
+    "South Africa",
+    "Spain",
+    "Sri Lanka",
+    "Sudan",
+    "Suriname",
+    "Swaziland",
+    "Sweden",
+    "Switzerland",
+    "Syria",
+    "Taiwan",
+    "Tajikistan",
+    "Tanzania",
+    "Thailand",
+    "Togo",
+    "Tonga",
+    "Trinidad and Tobago",
+    "Tunisia",
+    "Turkey",
+    "Turkmenistan",
+    "Tuvalu",
+    "Uganda",
+    "Ukraine",
+    "United Arab Emirates",
+    "United Kingdom",
+    "United States",
+    "Uruguay",
+    "Uzbekistan",
+    "Vanuatu",
+    "Vatican City",
+    "Venezuela",
+    "Vietnam",
+    "Yemen",
+    "Zambia",
+    "Zimbabwe",
+]
+n = len(countries)
+i = n // 2
+half = countries[i]
+print(half)
+# Divide the countries list into two equal lists if it is even if not one more country for the first half.
+first_half = countries[: i + 1]
+second_half = countries[i + 1 :]
+print(first_half)
+print(second_half)
+# ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark'].
+# Unpack the first three countries and the rest as scandic countries.
+countries = ["China", "Russia", "USA", "Finland", "Sweden", "Norway", "Denmark"]
+first_country, second_country, third_country, *scandic = countries
+print(first_country)
+print(second_country)
+print(third_country)
+print(scandic)
