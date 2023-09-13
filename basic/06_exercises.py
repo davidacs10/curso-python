c80e54461f811f3b80a4147d0a97047f124c5369
davidacs10 <davidacs10.08@gmail.com>
2023-09-13 09:57:24 -0400

    06_exercises.py complete

--- basic/06_exercises.py
+++ basic/06_exercises.py
@@ -21,6 +21,53 @@ it_companies.remove("Facebook")
 print(it_companies)
 
 # 5 What is the difference between remove and discard
-# The difference is the remove() method require a item exists. And discard no.
+# The difference is the remove() method require a item exists. And discard doesn't require it.
 it_companies.discard("Facebook")
 print(it_companies)
+
+# 6 Join A and B
+print(A.union(B))
+
+# 7 Find A intersection B
+print(A.intersection(B))
+
+# 8 Is A subset of B
+print(A.issubset(B))  # True
+
+# 9 Are A and B disjoint sets
+print(A.isdisjoint(B))
+
+# 10 Join A with B and B with A
+A.update(B)
+print(A)
+print(B.union(A))
+
+# 11 What is the symmetric difference between A and B
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+
+result = A.symmetric_difference(B)
+print(result)
+
+# 12 Delete the sets completely
+del A
+del B
+
+# 13 Convert the ages to a set and compare the length of the list and the set, which one is bigger?
+print(len(age))
+ageset = set(age)
+print(len(ageset))
+print(len(age) > len(ageset))
+
+# 14 Explain the difference between the following data types: string, list, tuple and set.
+# string: es una cadena de texto
+# list: es una cadena de elementos organizada, mutable y se pueden repetir los elementos
+# tuple: es una cadena de elementos organizada, no mutables y se pueden repetir los elementos
+# set: es una cadena de elementos no organizada, no mutables. Pero se pueden agregar nuevos elementos. No repetidos.
+
+# 15 I am a teacher and I love to inspire and teach people. How many unique words have been used in the sentence?
+# Use the split methods and set to get the unique words.
+teach = "I am a teacher and I love to inspire and teach people"
+words = teach.split()
+unique_words = len(set(words))
+print(unique_words)
