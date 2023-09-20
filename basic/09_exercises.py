a8fee75833eafc3343c044d11d41314ba9cfbd8a
davidacs10 <davidacs10.08@gmail.com>
2023-09-19 21:17:59 -0400

    09_exercises.py 2.0

--- basic/09_exercises.py
+++ basic/09_exercises.py
@@ -1,36 +1,59 @@
-# 1 Iterate 0 to 10 using for loop, do the same using while loop.
-for number in range(11):
-    print(number)
+# # 1 Iterate 0 to 10 using for loop, do the same using while loop.
+# for number in range(11):
+#     print(number)
 
-count = 0
-while count < 11:
-    print(count)
-    count = count + 1
+# count = 0
+# while count < 11:
+#     print(count)
+#     count = count + 1
 
-# 2 Iterate 10 to 0 using for loop, do the same using while loop.
-for number in range(10, -1, -1):
-    print(number)
+# # 2 Iterate 10 to 0 using for loop, do the same using while loop.
+# for number in range(10, -1, -1):
+#     print(number)
 
-count = 10
-while count >= 0:
-    print(count)
-    count = count - 1
+# count = 10
+# while count >= 0:
+#     print(count)
+#     count = count - 1
 
 # 3 Write a loop that makes seven calls to print(), so we get on the output the following triangle:
-for i in range(1, 8):
-    print("#" * i)
+# for i in range(1, 8):
+#     print("#" * i)
 
 # 4 Use nested loops to create the following:
-# La altura del triángulo.
-altura = 8
+# for _ in range(8):
+#     for _ in range(8):
+#         print("#", end=" ")
+#     print()
 
-# Creamos un bucle para iterar sobre las filas del triángulo.
-for i in range(altura):
-    # Creamos un bucle para iterar sobre los caracteres de cada fila.
-    for j in range(altura):
-        # Imprimimos un asterisco si el índice de la fila es menor o igual que el índice del carácter.
-        if i <= j:
-            print("#", end="")
-        else:
-            print(" ", end="")
-    print()
+# # 5 Print the following pattern:
+# for i in range(11):
+#     for _ in range(11):
+#         result = _ * i
+#         if _ != i:
+#             continue
+#         print(f"{_} x {i} =", result)
+
+# for i in range(11):  # Iteramos desde 0 hasta 10
+#     result = i * i  # Calculamos el resultado de la multiplicación
+#     print(f"{i} x {i} = {result}")
+
+# 6 Iterate through the list, ['Python', 'Numpy','Pandas','Django', 'Flask'] using a for loop and print out the items.
+# stack = ["Python", "Numpy", "Pandas", "Django", "Flask"]
+# for _ in stack:
+#     print(_)
+
+# 7 Use for loop to iterate from 0 to 100 and print only even numbers
+# for number in range(2, 102, 2):
+#     print(number)
+
+# 8 Use for loop to iterate from 0 to 100 and print only odd numbers
+# for number in range(0, 100):
+#     if number % 2 == 0:
+#         continue
+#     print(number)
+
+# 9 Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+number = 0
+for i in range(101):
+    print(number + i)
