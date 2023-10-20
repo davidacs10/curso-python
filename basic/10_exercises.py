f849f9b49a8687b974e741a8ff5782ec31e7a40e
davidacs10 <davidacs10.08@gmail.com>
2023-10-20 14:10:17 -0400

    10_exercises.py 2.0

--- basic/10_exercises.py
+++ basic/10_exercises.py
@@ -68,3 +68,88 @@ def check_season(month):
 
 
 print(check_season("december"))
+
+
+# 6 Write a function called calculate_slope which return the slope of a linear equation
+def calculate_slope(x1, y1, x2, y2):
+    return (y2 - y1) / (x2 - x1)
+
+
+x1 = 1
+y1 = 2
+x2 = 3
+y2 = 4
+
+slope = calculate_slope(x1, y1, x2, y2)
+
+print(slope)
+
+
+# 7 Quadratic equation is calculated as follows:
+# ax² + bx + c = 0. Write a function which calculates solution set of a quadratic equation, solve_quadratic_eqn.
+import math
+
+
+def solve_quadratic_eqn(a, b, c):
+    discriminant = b**2 - 4 * a * c
+    if discriminant < 0:
+        return set()
+    elif discriminant == 0:
+        return {-b / 2 * a}
+    else:
+        return {
+            (-b + math.sqrt(discriminant)) / (2 * a),
+            (-b - math.sqrt(discriminant)) / (2 * a),
+        }
+
+
+a = 1
+b = 2
+c = 3
+
+solutions = solve_quadratic_eqn(a, b, c)
+print(solutions)
+
+
+# 8 Declare a function named print_list. It takes a list as a parameter and it prints out each element of the list.
+def print_list(list):
+    for i in list:
+        print(i)
+
+
+vegetables = ["Letucce", "Tomato", "Onion", "Potato"]
+print_list(vegetables)
+
+
+# 9 Declare a function named reverse_list. It takes an array as a parameter and it returns the reverse of the array (use loops).
+def reverse_list(list):
+    reversed_list = []
+    for i in range(len(list) - 1, -1, -1):
+        reversed_list.append(list[i])
+    return reversed_list
+
+
+print(reverse_list(vegetables))
+
+
+# 10 Declare a function named capitalize_list_items. It takes a list as a parameter and it returns a capitalized list of items
+def capitalize_list_items(list):
+    capitalize_list = []
+    for i in list:
+        capitalize_list.append(i.upper())
+    return capitalize_list
+
+
+print(capitalize_list_items(vegetables))
+
+
+# 11 Declare a function named add_item. It takes a list and an item parameters. It returns a list with the item added at the end.
+def add_item(list, string):
+    list = []
+    for i in string:
+        list.append([i])
+    return list
+
+
+food_staff = ["Potato", "Tomato", "Mango", "Milk"]
+print(add_item(food_staff, "Meat"))
