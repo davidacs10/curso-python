005fe18c36a78bdb97bc279909c3d726dee2a8cb
davidacs10 <davidacs10.08@gmail.com>
2023-11-21 11:08:15 -0400

    exercises_03.py 1.1

--- exercises/exercises_03.py
+++ exercises/exercises_03.py
@@ -34,16 +34,57 @@ circle_area = pi * r**2
 circunference = 2 * pi * r
 print(circle_area)
 print(circunference)
-# 8 Calcula la pendiente, la intersección con el eje x y la intersección con el eje y de y = 2x -2
-# 9 La pendiente es (m = y2-y1/x2-x1). Halla la pendiente y la distancia euclidiana entre el punto (2, 2) y el punto (6,10)
-# 10 Compara las pendientes de las tareas 8 y 9.
-# 11 Calcula el valor de y (y = x^2 + 6x + 9). Intenta usar diferentes valores de x y averigua en qué valor de x y va a ser 0.
-# 12 Encuentra la longitud de 'pitón' y 'dragón' y haz una afirmación de comparación falsa.
-# 13 Use y para comprobar si 'on' se encuentra tanto en 'python' como en 'dragon'
-# 14 Espero que este curso no esté lleno de jerga. Use el operador in para verificar si hay jerga en la oración.
-# 15 No hay 'encendido' tanto en el dragón como en la pitón
-# 16 Encuentre la longitud del texto python y convierta el valor a float y conviértalo en cadena
-# 17 Los números pares son divisibles por 2 y el resto es cero. ¿Cómo se comprueba si un número es par o no usando python?
-# 18 Compruebe si la división del piso de 7 por 3 es igual al valor int convertido de 2.7.
-# 19 Compruebe si el tipo de '10' es igual al tipo de 10
-# 20 Compruebe si int('9.8') es igual a 10
+# 8 Encuentra la longitud de 'python' y 'dragon' y haz una afirmación de comparación falsa.
+word_1 = "python"
+word_2 = "dragon"
+print(len(word_1))
+print(len(word_2))
+print(len(word_1) is not len(word_2))
+# 9 Use y para comprobar si 'on' se encuentra tanto en 'python' como en 'dragon'
+comparation = "on" in word_1 and "on" in word_2
+print(comparation)
+# 10 Espero que este curso no esté lleno de jerga. Use el operador in para verificar si hay jerga en la oración.
+phrase_1 = "I hope this course is not full of jargon"
+print("jargon" in phrase_1)
+# 11 No hay 'on' tanto en 'python' y 'dragon'
+print("on" not in word_1 and "on" not in word_2)
+# 12 Encuentre la longitud del texto python y convierta el valor a float y conviértalo en cadena
+word_1 = len(word_1)
+word_1 = float(word_1)
+word_1 = str(word_1)
+print(word_1)
+# 13 Los números pares son divisibles por 2 y el resto es cero. ¿Cómo se comprueba si un número es par o no usando python?
+number = 4
+even = number % 2 == 0
+odd = number % 2 == 1
+print(even)
+print(odd)
+# 14 Compruebe si la división del piso de 7 por 3 es igual al valor int convertido de 2.7.
+floor_division = 7 // 3
+value = int(2.7)
+print(floor_division == value)
+# 15 Compruebe si el tipo de '10' es igual al tipo de 10
+type_value_1 = "10"
+type_value_2 = 10
+print(type_value_1 == type_value_2)
+# 16 Compruebe si int('9.8') es igual a 10
+value_1 = int(float("9.8"))
+value_2 = 10
+print(value_1 == value_2)
+# 17 Escriba un script que solicite al usuario que ingrese las horas y la tarifa por hora. ¿Calcular el salario de la persona?
+hours = int(input("Enter hours: "))
+rate = int(input("Enter hours: "))
+earning = hours * rate
+print(f"Your weekly earning is: {earning}")
+# 18 Escriba un script que pida al usuario que introduzca el número de años. Calcula el número de segundos que una persona puede vivir. Supongamos que una persona puede vivir cien años
+years = int(input("Numbers of years: "))
+seconds = 31536000
+seconds_lived = years * seconds
+print(f"You have lived for {seconds_lived} seconds.")
+# 19 Escriba un script de Python que muestre la siguiente tabla
+first_row = "1 1 1 1 1"
+second_row = "2 1 2 4 8"
+third_row = "3 1 3 9 27"
+fourth_row = "4 1 4 16 64"
+fifth_row = "5 1 5 25 125"
+print(f"{first_row}\n{second_row}\n{third_row}\n{fourth_row}\n{fifth_row}")
