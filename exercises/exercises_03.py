439af644891e1db515c007ccbd155ee287f617fa
davidacs10 <davidacs10.08@gmail.com>
2023-11-20 20:11:50 -0400

    exercises_03.py

---
+++ exercises/exercises_03.py
@@ -0,0 +1,49 @@
+# Operadores
+# 1 Declara tu edad como variable entera
+age = 23
+age = int(age)
+# 2 Declarar la altura como una variable flotante
+height = 1.79
+height = float(height)
+# 3 Declarar una variable que almacene un número complejo
+complex_number = 4j
+complex_number = complex(4j)
+# 4 Escriba un script que solicite al usuario que introduzca la base y la altura del triángulo y calcule un área de este triángulo (área = 0,5 x b x h).
+base = int(input("Base: "))
+height = int(input("Height: "))
+area = 0.5 * base * height
+area = int(area)
+print(f"The area of the circle is: {area}")
+# 5 Escriba un script que pida al usuario que escriba el lado a, el lado b y el lado c del triángulo. Calcula el perímetro del triángulo (perímetro = a + b + c).
+side_a = int(input("Enter side a:"))
+side_b = int(input("Enter side b:"))
+side_c = int(input("Enter side c:"))
+perimeter = side_a + side_b + side_c
+print(perimeter)
+# 6 Obtenga la longitud y el ancho de un rectángulo usando el mensaje. Calcula su área (área = largo x ancho) y perímetro (perímetro = 2 x (largo + ancho))
+height = int(input("Enter height:"))
+width = int(input("Enter width:"))
+rectangle_area = height * width
+print(rectangle_area)
+rectangle_perimeter = 2 * (height + width)
+print(rectangle_perimeter)
+# 7 Obtener el radio de un círculo usando el mensaje. Calcule el área (área = pi x r x r) y la circunferencia (c = 2 x pi x r) donde pi = 3.14.
+pi = 3.14
+r = int(input("Enter radius:"))
+circle_area = pi * r**2
+circunference = 2 * pi * r
+print(circle_area)
+print(circunference)
+# 8 Calcula la pendiente, la intersección con el eje x y la intersección con el eje y de y = 2x -2
+# 9 La pendiente es (m = y2-y1/x2-x1). Halla la pendiente y la distancia euclidiana entre el punto (2, 2) y el punto (6,10)
+# 10 Compara las pendientes de las tareas 8 y 9.
+# 11 Calcula el valor de y (y = x^2 + 6x + 9). Intenta usar diferentes valores de x y averigua en qué valor de x y va a ser 0.
+# 12 Encuentra la longitud de 'pitón' y 'dragón' y haz una afirmación de comparación falsa.
+# 13 Use y para comprobar si 'on' se encuentra tanto en 'python' como en 'dragon'
+# 14 Espero que este curso no esté lleno de jerga. Use el operador in para verificar si hay jerga en la oración.
+# 15 No hay 'encendido' tanto en el dragón como en la pitón
+# 16 Encuentre la longitud del texto python y convierta el valor a float y conviértalo en cadena
+# 17 Los números pares son divisibles por 2 y el resto es cero. ¿Cómo se comprueba si un número es par o no usando python?
+# 18 Compruebe si la división del piso de 7 por 3 es igual al valor int convertido de 2.7.
+# 19 Compruebe si el tipo de '10' es igual al tipo de 10
+# 20 Compruebe si int('9.8') es igual a 10
