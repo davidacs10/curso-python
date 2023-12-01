5a11d1d017001f0fe7755a0597185d56910883ed
davidacs10 <davidacs10.08@gmail.com>
2023-11-30 21:53:18 -0400

    exercises_10.py 2.0

--- exercises/exercises_10.py
+++ exercises/exercises_10.py
@@ -49,7 +49,7 @@ for i in range(101):
 total = 0
 for i in range(101):
     total += i
-    print(f"The sum of all numbers is {total}.")
+print(f"The sum of all numbers is {total}.")
 # 10 Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
 even_sum = 0
 odd_sum = 0
@@ -68,3 +68,45 @@ for i in countries:
     if i.endswith("land"):
         land.append(i)
 print(land)
+# 12 This is a fruit list, ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop.
+fruit = ["banana", "orange", "mango", "lemon"]
+reverse_fruit_list = []
+for i in reversed(fruit):
+    reverse_fruit_list.append(i)
+print(reverse_fruit_list)
+# 13 Go to the data folder and use the countries_data.py file.
+import json
+from collections import Counter
+
+with open("data/countries_data.json", "r") as file:
+    data = json.load(file)
+# 14 What are the total number of languages in the data
+total_languages = set()
+for laguages in data:
+    total_languages.update(laguages["languages"])
+print(len(total_languages))
+# 15 Find the ten most spoken languages from the data
+ten_languages = []
+for languages in data:
+    ten_languages.extend(languages["languages"])
+
+# Contabiliza la frecuencia de cada lenguaje
+most_spoken_language = Counter(ten_languages)
+# Obtiene los 10 lenguajes más comunes
+top_10_languages = most_spoken_language.most_common(10)
+print(top_10_languages)
+# Imprime los resultados
+print("Los 10 lenguajes más hablados:")
+for language, count in top_10_languages:
+    print(f"{language}: {count}")
+# 16 Find the 10 most populated countries in the world
+most_populated = []
+for country_data in data:
+    most_populated.append((country_data["name"], country_data["population"]))
+# Contabiliza la frecuencia de cada país
+most_populated_country = Counter(dict(most_populated))
+# Obtiene los 10 países con mayor población
+top_10_populated_countries = most_populated_country.most_common(10)
+print("Los 10 países más poblados del mundo:")
+for countries, population in top_10_populated_countries:
+    print(f"{countries}: {population}")
