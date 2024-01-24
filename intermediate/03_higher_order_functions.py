8dfff2a8fa084e1bb9dca878ee6fb04d365cc272
davidacs10 <davidacs10.08@gmail.com>
2024-01-24 09:54:47 -0400

    03_higher_order_functions.py 2.0

--- intermediate/03_higher_order_functions.py
+++ intermediate/03_higher_order_functions.py
@@ -121,3 +121,131 @@ def hello_python():
     return "Hello Python"
 
 print(hello_python())
+
+# Aceptación de parámetros en funciones de decorador
+
+def decorator_with_parameters(function):
+    def wrapper_accepting_parameters(para1, para2, para3):
+        function(para1, para2, para3)
+        print(f"I live in {para3}")
+    return wrapper_accepting_parameters
+
+@decorator_with_parameters
+def print_full_name(first_name, last_name, country):
+    print(f"I am {first_name} {last_name}. I love to learn.")
+
+print_full_name("David", "Campos", "Venezuela")
+
+# Funciones de orden superior incorporadas
+
+"""Algunas de las funciones integradas de orden superior que cubrimos en esta parte son:
+map(), filter() y reduce(). La función lambda se puede pasar como parámetro 
+y el mejor caso de uso de las funciones lambda es en funciones como map(), filter() y reduce()."""
+
+"""
+Python - Función de mapa
+Se utiliza para aplicar una función a todos los elementos de una o más secuencias (listas, tuplas, etc.).
+y devuelve un iterador que produce los resultados.
+"""
+
+"""
+Sintaxis
+map(function, iterable)
+"""
+
+# Ejemplo 1
+numbers = [1, 2, 3, 4, 5]
+def square(x):
+    return x ** 2
+numbers_squared = map(square, numbers)
+print(list(numbers_squared))
+
+# Apliquemos este ejemplo con lambda
+numbers_squared = map(lambda x : x ** 2, numbers)
+print(list(numbers_squared))
+
+# Ejemplo 2
+numbers_str = ["1", "2", "3", "4", "5"]
+print(numbers_str)
+numbers_int = map(int, numbers_str)
+print(list(numbers_int))
+
+# Ejemplo 3
+names = ["David", "Alejandro", "Campos", "Specht"]
+
+def change_to_upper(name):
+    return name.upper()
+
+change_to_uppercase = map(change_to_upper, names)
+print(list(change_to_uppercase))
+
+# Apliquemos este ejemplo con lambda
+change_to_uppercase = map(lambda name: name.upper(), names)
+print(list(change_to_uppercase))
+
+"""
+Lo que hace este map es iterar sobre una lista de nombres
+los cambia a mayusculas y devuelve una lista con nombres en mayuscula
+"""
+
+"""
+Python - Función de filtro
+Se utiliza para filtrar elementos de una secuencia (como una lista)
+basándose en una función que devuelve valores booleanos (True o False).
+"""
+
+"""
+Sintaxis
+filter(function, iterable)
+"""
+
+# Ejemplo 1
+# Vamos a filtrar solo los numeros pares
+numbers = [1, 2, 3, 4, 5]
+
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    return False
+
+only_even_numbers = filter(is_even, numbers)
+print(list(only_even_numbers))
+
+# Ejemplo 2
+numbers = [1, 2, 3, 4, 5]
+
+def is_odd(num):
+    if num % 2 != 0:
+        return True
+    return False
+
+only_odd_numbers = filter(is_odd, numbers)
+print(list(only_odd_numbers))
+
+# Ejemplo 3
+names = ["David", "Jose", "Alejandro", "Mathias", "Abraham"]
+
+def is_long_name(name):
+    if len(name) > 5:
+        return True
+    return False
+
+is_long = filter(is_long_name, names)
+print(list(is_long))
+
+"""
+Python - Función de reducción
+La función reduce() está definida en el módulo functools y debemos importarla desde este módulo. 
+Al igual que el mapa y el filtro, toma dos parámetros, una función y un iterable. 
+Sin embargo, no devuelve otro iterable, sino que devuelve un único valor.
+"""
+
+from functools import reduce
+
+# Ejemplo 1
+numbers_str = ["1", "2", "3", "4", "5"]
+def sum_numbers(x, y):
+    return int(x) + int(y)
+
+total = reduce(sum_numbers, numbers_str)
+print(total)
