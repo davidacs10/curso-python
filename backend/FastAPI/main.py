e99eedb62d6c6baeeb908b0ecb6646a3ec203c8a
davidacs10 <davidacs10.08@gmail.com>
2024-02-09 11:35:34 -0400

    First steps with FastAPI

---
+++ backend/FastAPI/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+# Esta app es la misma a la que nos referimos cuando usamos el comando de uvicorn
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+"""             EN RESUMEN
+Importa FastAPI. # Linea 1
+Crea un instance de app. # Linea 4
+Escribe un decorador de operación de path (como @app.get("/")). # Linea 6
+Escribe una función de la operación de path (como def root(): ... arriba). # Linea 7
+Corre el servidor de desarrollo (como uvicorn main:app --reload). # Esto lo ejecutas en consola
+"""
