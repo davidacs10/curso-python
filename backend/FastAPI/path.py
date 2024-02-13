0a9b471cd440235cd47b96a45b08e26c52d0cc9c
davidacs10 <davidacs10.08@gmail.com>
2024-02-13 09:12:56 -0400

    minor fixed

--- backend/FastAPI/path.py
+++ backend/FastAPI/path.py
@@ -27,7 +27,20 @@ async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
     
-    if ModelName.lenet.value:
+    if model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
     
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+"""
+la ruta /files/documentos/archivo.txt coincidiría con la ruta 
+definida en @app.get("/files/{file_path:path}"), y la parte 
+documentos/archivo.txt sería capturada como el valor del parámetro 
+file_path. Luego, este valor se devuelve como una respuesta JSON 
+que contiene la clave "file_path" y el valor "documentos/archivo.txt"
+"""
