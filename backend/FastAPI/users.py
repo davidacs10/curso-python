a31c302e974650963595d3169a6fc0471247cc2c
davidacs10 <davidacs10.08@gmail.com>
2024-02-15 15:09:34 -0400

    HTTP status code

--- backend/FastAPI/users.py
+++ backend/FastAPI/users.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 class User(BaseModel):
@@ -33,10 +33,10 @@ async def user(id: int):
     return search_user(id)
 
 # Create
-@app.post("/user/")
+@app.post("/user/", status_code=201)
 async def user(user: User):
     if type(search_user(user.id)) == User:
-        return {"error": "El usuario ya existe"}
+        raise HTTPException(status_code=409, detail="El usuario ya existe.")
     user_list.append(user)
     return user
 
