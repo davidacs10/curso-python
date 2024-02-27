c8f3f00918ba03e20f9c026299ebfd830524ebc6
davidacs10 <davidacs10.08@gmail.com>
2024-02-27 18:40:54 -0400

    MongoDB FastAPI 0.1.0

--- backend/FastAPI/routers/users_db.py
+++ backend/FastAPI/routers/users_db.py
@@ -1,31 +1,30 @@
 from fastapi import APIRouter, HTTPException, status
 from db.models.user import User
-from db.schemas.user import user_schema
+from db.schemas.user import user_schema, users_schema
 from db.client import db_client
+from bson import ObjectId
 
-
-user_list = []
 
 router = APIRouter(prefix="/usersdb", 
                    tags= ["usersdb"], 
                    responses= {status.HTTP_404_NOT_FOUND:{"message":"No encontrado"}})
 
-@router.get("/")
+@router.get("/", response_model=list[User])
 async def get_users():
-    return db_client.users.find()
+   return users_schema(db_client.users.find())
 
-@router.get("{id}")
-async def user(id: int):
-        return search_user(id)
+@router.get("/{id}")
+async def user(id: str):
+    return search_user("_id", ObjectId(id))
 
 @router.get("/")
-async def user(id: int):
-    return search_user(id)
+async def user(id: str):
+    return search_user("_id", ObjectId(id))
 
 # Create
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
 async def user(user: User):
-    if type(search_user_by_email(user.email)) == User:
+    if type(search_user("email",user.email)) == User:
         raise HTTPException(status_code=409, detail="El usuario ya existe.")
 
     user_dict = dict(user)
@@ -38,52 +37,42 @@ async def user(user: User):
     return User(**new_user)
 
 # Update
-@router.put("/")
+@router.put("/", response_model=User)
 async def user(user: User):
 
-    found = False
+    user_dict = dict(user)
+    del user_dict["id"]
 
-    for index, saved_user in enumerate(user_list):
-        if saved_user.id == user.id:
-            user_list[index] = user
-            found = True
-    
-    if not found:
-        raise HTTPException(status_code=304, 
-                            detail={"Error":"No se ha encontrado el usuario"}, 
-                            headers={"304":"Usuario no encontrado"})
-
-    return user
+    try:
+        db_client.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
+    except:
+        raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
+                      detail="No se ha actualizado el usuario",
+                      headers={"405": "No actualizado"})
+        
+    return search_user("_id", ObjectId(user.id))
 
 # Delete
 @router.delete("/{id}")
-async def user(id: int):
-    found = False
-
-    for index, saved_user in enumerate(user_list):
-        if saved_user.id == id:
-            del user_list[index]
-            found = True
+async def user(id: str):
+    found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
     
     if not found:
-        raise HTTPException(status_code=405, 
+        raise HTTPException(status_code=status.HTTP_405_METHOD_NOT_ALLOWED, 
                             detail="El usuario no existe", 
                             headers={"405": "El usuario no existe"})
     
-    raise HTTPException(status_code=200, 
-                            detail="Usuario eliminado con exito", 
-                            headers={"200": "Usuario eliminado con exito"})
+    raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, 
+                            headers={"204": "Usuario eliminado con exito"})
 
 
 # Funcion para buscar email
-def search_user_by_email(email: str):
+def search_user(field: str, key):
     try:
-        user = db_client.users.find_one({"email":email})
-        return User(**user_schema(user))
-    except:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-                            detail="User not found", 
-                            headers={"404": "User not found"})
-
-async def search_user():
-    pass
+        user = db_client.users.find_one({field:key})
+        if user:
+            return User(**user_schema(user))
+        else:
+            return None
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500, detail=f"Error searching user: {e}")
